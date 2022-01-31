ebf47016a6c66299914486a023e32c24bb4a4c22
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-01-31 12:14:14 -0300

    calculadora imc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './app.css'
 
-function App() {
-  return (
+export default  function app (){
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC (){
+    const alt = altura / 100
+    const resultado = peso / (alt * alt)
+
+    const imc = resultado.toFixed(2)
+
+    if(imc < 18.5){
+      setMensagem(`você está abaixo do peso e seu imc é ${imc}`)
+    }else if (imc >= 18.5 && imc < 24.9){
+     setMensagem(`você está com o peso normal e seu imc é ${imc}`)
+    }else if(imc >= 24.9 && imc < 30 ){
+     setMensagem(`você está com sobre peso e seu imc é ${imc}`)
+
+    }else if(imc >= 30 ){
+      setMensagem(`você está obeso e seu imc é ${imc}`)
+    }
+
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu imc</span>
 
-export default App;
+      <div className="area-input">
+          <input
+            type="text"
+
+            value={peso}
+            onChange={ (e) => setPeso(e.target.value) }
+            placeholder="Peso em (kg) ex: 90"
+          />
+          <input
+            type="text"
+            value={altura}
+            onChange={ (e) => setAltura(e.target.value) }
+            placeholder="Altura em (cm) ex: 173"
+          />
+
+
+          <button onClick={calcularIMC}> 
+            calcular
+          </button>
+      </div>
+
+      <h2>{mensagem}</h2>
+    </div>
+  )
+}
